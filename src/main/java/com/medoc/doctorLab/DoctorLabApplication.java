1d28e59b5ae67f531098dcc67ae1c8f02bc43ba6
KajalSet <kajals@solwyz.com>
2025-02-06 12:10:55 +0530

    initial commit

---
+++ src/main/java/com/medoc/doctorLab/DoctorLabApplication.java
@@ -0,0 +1,13 @@
+package com.medoc.doctorLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medoc/doctorLab/DoctorLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medoc.doctorLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
