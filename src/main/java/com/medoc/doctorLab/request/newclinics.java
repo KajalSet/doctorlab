fe12f18abc6cb5749919a74b0b2b6ee6e114a55b
KajalSet <kajals@solwyz.com>
2025-02-06 14:55:54 +0530

    new

---
+++ src/main/java/com/medoc/doctorLab/request/newclinics.java
@@ -0,0 +1,13 @@
+package com.medoc.doctorLab.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class newclinics {
+	private String name;
+    private String address;
+    private double rating;
+    private double distance;
+}
